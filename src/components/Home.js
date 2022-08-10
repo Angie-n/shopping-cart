2760f610bcb49d44f2ce52051110e6f7b379450d
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-08-10 16:31:31 -0700

    Our values div, import styles for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,36 @@
 import Carousel from "./Carousel";
+import "../styles/Home.css";
+
+const OurValues = () => {
+    let index = 0;
+
+    const createValueDiv = (icon, header, text) => {
+        index++;
+        return (<div key={"our-values-" + index}>{icon}<h3>{header}</h3><p>{text}</p></div>);
+    };
+
+    const fillOurValuesDivs = () => {
+        let values = [];
+        values.push(createValueDiv(<i className="fa-solid fa-truck"></i>, "Delivery guaranteed", "Unsure about fast, but it'll come eventually."));
+        values.push(createValueDiv(<i className="fa-solid fa-book"></i>, "Free plant care guides", "So many have died"));
+        values.push(createValueDiv(<i className="fa-solid fa-heart"></i>, "Made with love", "Try and prove it's wrong, we'll wait"));
+        return values;
+    }
+
+    const ourValuesDivs = fillOurValuesDivs();
+    
+    return (
+        <div id="our-values-container">
+            {ourValuesDivs.map(vd => vd)}
+        </div>
+    );
+}
 
 const Home = () => {
     return (
         <main>
             <Carousel></Carousel>
+            <OurValues></OurValues>
         </main>
     );
 }
