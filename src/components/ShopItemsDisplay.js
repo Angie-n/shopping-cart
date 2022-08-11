66fba8d511c1e0c52edffb39cf245daf66209064
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-08-11 14:52:57 -0700

    Initial products now made in helper instead of component

--- src/components/ShopItemsDisplay.js
+++ src/components/ShopItemsDisplay.js
@@ -1,34 +1,8 @@
+import { Link } from "react-router-dom";
 import "../styles/ShopItemsDisplay.css";
 
-function importAll(r) {
-    return r.keys().map(r);
-}
-  
-const images = importAll(require.context('../assets/images/shop', true, /\.(png|jpe?g)$/));
-
-const Product = (name, description, image, price, categories) => {
-    return {name, description, image, price, categories};
-}
-
-const createProducts = () => {
-    let products = [];
-    const defaultDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    const defaultPrice = 10000;
-    images.forEach(img => {
-        let indexOfStaticMedia = img.indexOf("static/media");
-        let imgNoStaticMedia = img.substring(indexOfStaticMedia + 13);
-        let endOfName = imgNoStaticMedia.indexOf(".");
-        let name = imgNoStaticMedia.substring(0, 1).toUpperCase() + imgNoStaticMedia.substring(1, endOfName);
-        name = name.replaceAll("-", " ");
-        products.push(Product(name, defaultDescription, img, defaultPrice, []));
-    });
-    return products;
-}
-
-const products = createProducts();
-
 const ShopItemsDisplay = props => {  
-    const {cartItems, setCartItems} = props;
+    const {allProducts, cartItems, setCartItems} = props;
 
     const handleProductMouseEnter = e => {
         e.currentTarget.getElementsByTagName("img")[0].classList.add("half-fade-out");
@@ -52,15 +26,16 @@ const ShopItemsDisplay = props => {
 
     return (
         <div id="product-container">
-            {products.map((p, i) => {return <div key={"product-" + i} onMouseEnter={e =>handleProductMouseEnter(e)} onMouseLeave={e => handleProductMouseLeave(e)}>
+            {allProducts.map((p, i) => {return <div key={"product-" + i} onMouseEnter={e =>handleProductMouseEnter(e)} onMouseLeave={e => handleProductMouseLeave(e)}>
+                    <Link to={p.pageName}></Link>
                     <img src={p.image} alt=""></img>
                     <div>
                         <h3>{p.name}</h3><p>${p.price}</p>
                         <form method="post" className="quick-add-container" onSubmit={e => handleQuickAddButtonClick(e, p)}>
                             <label htmlFor="quick-add"><button type="submit">Quick Add</button></label>
-                            <input type="number" name="quick-add" defaultValue="0" min="0" onInput={(e) => e.currentTarget.value = Math.abs(e.currentTarget.value)}></input>
-                            <button type="button" className="quick-add-fine-tuning" onClick={() => document.querySelectorAll("input[name='quick-add'")[i].value++}>+</button>
-                            <button type="button" className="quick-add-fine-tuning" onClick={() => {if(document.querySelectorAll("input[name='quick-add'")[i].value > 0) document.querySelectorAll("input[name='quick-add'")[i].value--}}>-</button>
+                            <input type="number" name="quick-add" defaultValue="0" min="0" onInput={e => e.currentTarget.value = Math.abs(e.currentTarget.value)}></input>
+                            <button type="button" className="fine-tuning-btn" onClick={() => {document.querySelectorAll("input[name='quick-add'")[i].value++}}>+</button>
+                            <button type="button" className="fine-tuning-btn" onClick={() => {if(document.querySelectorAll("input[name='quick-add'")[i].value > 0) document.querySelectorAll("input[name='quick-add'")[i].value--}}>-</button>
                         </form>
                     </div>
                 </div>})}
---
+++ src/helpers/Product.js
@@ -0,0 +1,28 @@
+function importAll(r) {
+    return r.keys().map(r);
+}
+  
+const images = importAll(require.context('../assets/images/shop', true, /\.(png|jpe?g)$/));
+
+const Product = (name, description, image, price, categories, pageName) => {
+    return {name, description, image, price, categories, pageName};
+}
+
+const createProducts = () => {
+    let products = [];
+    const defaultDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const defaultPrice = 10000;
+    images.forEach(img => {
+        let indexOfStaticMedia = img.indexOf("static/media");
+        let imgNoStaticMedia = img.substring(indexOfStaticMedia + 13);
+        let endOfName = imgNoStaticMedia.indexOf(".");
+        let pageName = imgNoStaticMedia.substring(0, endOfName);
+        let name = pageName.replaceAll("-", " ");
+        products.push(Product(name, defaultDescription, img, defaultPrice, [], pageName));
+    });
+    return products;
+}
+
+const products = createProducts();
+
+export default products;
